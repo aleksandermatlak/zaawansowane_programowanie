090e9c94da4ee4775a63f6a24c43b8f7be1dfed2
Alex <aleksander.matlak@edu.uekat.pl>
2022-11-08 05:53:46 +0100

    pep8 standards applied

--- zad_2.py
+++ zad_2.py
@@ -1,13 +1,15 @@
-#a
+# a
 def names(firstnames):
     for x in firstnames:
         print(x)
 
-imiona = ['Aleksander','Stansisław','Michał','Maciej','Czesław']
+
+imiona = ['Aleksander', 'Stansisław', 'Michał', 'Maciej', 'Czesław']
 
 names(imiona)
 
-#b
+
+# b
 def multiplication(liczby):
     num = []
     for x in liczby:
@@ -17,18 +19,19 @@ def multiplication(liczby):
 
 
 def multiplication_comprehension(liczby):
-    num = [x*2 for x in liczby]
+    num = [x * 2 for x in liczby]
     return num
 
 
-numbers = [4,3,6,2,12]
+numbers = [4, 3, 6, 2, 12]
 
 result = multiplication(numbers)
 result2 = multiplication_comprehension(numbers)
 print(result)
 print(result2)
 
-#c
+
+# c
 def even_numbers(numbers):
     for x in range(10):
         if numbers[x] % 2 == 0:
@@ -36,15 +39,18 @@ def even_numbers(numbers):
         else:
             continue
 
-numbers_2=[1,2,3,4,5,6,7,8,9,10]
+
+numbers_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 even_numbers(numbers_2)
 
-#d
+
+# d
 def second_numbers(numbers):
-    for x in range(0,10,2):
+    for x in range(0, 10, 2):
         print(numbers[x])
 
-numbers_2=[1,2,3,4,5,6,7,8,9,10]
+
+numbers_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 second_numbers(numbers_2)
