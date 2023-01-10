3f2d651dc3d3a082a1b17a31ccaee7951f2fe83c
Alex <aleksander.matlak@edu.uekat.pl>
2023-01-10 11:50:07 +0100

    Minor code refactoring.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.person_detector import detection_model, analyze_images
+from src.person_detector import detection_model, analyze_image
 images = ['data_input/people1.jpg', 'data_input/people2.jpg', 'data_input/people3.jpg']
 
 model = detection_model()
